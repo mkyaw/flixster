c674bb05830d82f54828c401d5f972b6410dbff0
Myo Kyaw <myotkyaw@gmail.com>
2015-07-22 22:57:55 +0000

    Add tests to test course validation

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -16,6 +16,28 @@ class CourseTest < ActiveSupport::TestCase
     #course.cost = 20
     assert course.save
   end
-=end  
+=end
+
+  test "Course should not allow a course without a title to be created" do
+    course = Course.new(:title => nil, :description => 'omg', :cost => 20)
+    course.valid?
+    problems_with_course_title = course.errors[:title]
+    has_error = problems_with_course_title.present?
+    assert_equal true, has_error
+  end
+
+  test "Course, should be allowed to be created if it has a good title (and good other stuff too)" do
+    course = Course.new(:title => "title", :description => "omg", :cost => 20)
+    course.valid?
+
+#  {:title => [‘isn’t there’] }
+#  { }
+
+    problems_with_course_title = course.errors[:title]
+    has_error = problems_with_course_title.present?
+    assert_equal false, has_error
+end
+
+
 
 end
