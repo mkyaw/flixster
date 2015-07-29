35e8803b703c571f867bc6d3109afec2be992c73
Myo Kyaw <myotkyaw@gmail.com>
2015-07-29 07:10:13 +0000

    Set up course detail page - lesson page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
   resources :courses, :only => [:index, :show]
+  resources :lessons, :only => [:show]
   namespace :instructor do
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
