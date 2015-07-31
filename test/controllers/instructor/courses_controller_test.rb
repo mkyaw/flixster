01f875ea9928d75b6b72865a4ce10051a373ee64
Myo Kyaw <myotkyaw@gmail.com>
2015-07-31 23:42:31 +0000

    Fix before_action to require authorized user for show action

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -1,13 +1,20 @@
 require 'test_helper'
 
 class Instructor::CoursesControllerTest < ActionController::TestCase
+
+  def user
+    # @user = @user || FactoryGirl.create(:user)
+    @user ||= FactoryGirl.create(:user)
+  end
+
+  def setup
+    sign_in user
+  end
   
   test "create button creates a new course in database" do
-    user = FactoryGirl.create(:user)
-    sign_in user
-    
     assert_difference 'Course.count' do
       post :create, :course => {
+        :title => 'The title',
         :name => 'Hola',
         :description => 'Hola means hello',
         :cost => 20
@@ -19,6 +26,20 @@ class Instructor::CoursesControllerTest < ActionController::TestCase
     
     #assert_equal user, c.user
     assert_equal 1, user.courses.count
+  end
+
+  test "create button does not create a new course when course is invalid" do
+    assert_no_difference 'Course.count' do
+      # This is invalid because title is missing
+      post :create, :course => {
+        :name => 'Hola',
+        :description => 'Hello hello hello',
+        :cost => 70
+      }
+    end
+
+    assert_template :new
+    assert_response :unprocessable_entity
   end
 
 end
