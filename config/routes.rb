bf4cb269e3fa441b9887f3ae522f4de6bb20d95f
Myo Kyaw <myotkyaw@gmail.com>
2015-08-06 23:30:09 +0000

    Store student enrollment in database

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,20 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
-  resources :courses, :only => [:index, :show]
+  resources :courses, :only => [:index, :show] do
+    resources :enrollments, :only => :create
+  end
+  
   resources :lessons, :only => [:show]
+  
   namespace :instructor do
+    
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
     end
+    
     resources :courses, :only => [:new, :create, :show] do
+      
       resources :sections, :only => [:new, :create] do
         resources :lessons, :only => [:new, :create]
       end
---
+++ test/factories/enrollments.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :enrollment do
+    user_id 1
+course_id 1
+  end
+
+end
