5c25e9b404ab7c3c1b643dd0e71b7a5652c34089
Myo Kyaw <myotkyaw@gmail.com>
2015-08-08 01:13:28 +0000

    Set up student enrollment and check if already enrolled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,40 @@
 class User < ActiveRecord::Base
 	has_many :courses
   has_many :enrollments
+
+### refactoring one last time using has_many :through relation
+### to replace "enrolled_courses = enrollments.collect(&:course)"
+  has_many :enrolled_courses, :through => :enrollments, :source => :course
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def enrolled_in?(course)
+    
+### refactoring once more using "&:" and "collect"
+    # enrolled_courses = enrollments.collect(&:course)
+
+
+### refactoring code below using "collect" instead of "each"
+    
+=begin
+    enrolled_courses = enrollments.collect do |enrollment|
+      enrollment.course
+    end
+=end
+
+### code before refactoring
+
+=begin
+    enrolled_courses = []
+    enrollments.each do |enrollment|
+      enrolled_courses << enrollment.course
+    end
+
+=end
+    
+    return enrolled_courses.include?(course)
+  end
 end
