7ef73ccd59f7599ab349cb2458c485f96eef3c6a
Myo Kyaw <myotkyaw@gmail.com>
2015-08-10 14:15:30 +0000

    Require users to be enrolled in a course to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson_course, :only => [:show]
+
   def show
     
   end
 
   private
 
+  def require_authorized_for_current_lesson_course
+    if current_user.enrolled_in?(current_lesson.section.course) != current_user
+      redirect_to course_path(current_lesson.section.course), :alert => "Unauthorized to view the lesson content"
+    end
+  end
+
   helper_method :current_lesson
+  
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
