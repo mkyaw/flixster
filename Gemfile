d0525245c1ad29a14ca50845aef29ef25663b995
Myo Kyaw <myotkyaw@gmail.com>
2015-08-25 03:45:00 +0000

    Reorder lessons with AJAX drag and drop

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
 
 gem 'ranked-model'
 
+gem 'jquery-ui-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,13 @@ Rails.application.routes.draw do
   
   namespace :instructor do
     
+    resources :lessons, :only => [:update]
+
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
     end
     
     resources :courses, :only => [:new, :create, :show] do
-      
       resources :sections, :only => [:new, :create] do
         resources :lessons, :only => [:new, :create]
       end
